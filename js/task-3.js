1c6ba12020f74914234225bb6004ed77aef96e83
Yura Petryna <yur4ik911@gmail.com>
2024-11-05 10:40:36 +0200

    hm

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,7 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
 
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+inputEl.addEventListener("input", (event) => {
+  const trimmedValue = event.target.value.trim();
+  outputEl.textContent = trimmedValue || "Anonymous";
+});
---
+++ js/task-6.js
@@ -0,0 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value.trim(), 10);
+  if (amount >= 1 && amount <= 100) {
+    boxesContainer.innerHTML = "";
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = "5px";
+    boxes.push(box);
+  }
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
